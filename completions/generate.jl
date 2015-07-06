fe5339d3a6f1cb0b0e4cf37ef470a31889997695
Mike J Innes <mike.j.innes@gmail.com>
2015-07-06 11:55:36 +0100

    generate completions from Julia

---
+++ completions/generate.jl
@@ -0,0 +1,11 @@
+# Generate completions.json
+
+open(joinpath(dirname(@__FILE__), "completions.json"), "w") do io
+  println(io, "{")
+  for (word, char) in Base.REPLCompletions.latex_symbols
+    println(io, "  \"$(word[2:end])\": \"$char\",")
+  end
+  skip(io, -2)
+  println(io)
+  println(io, "}")
+end
